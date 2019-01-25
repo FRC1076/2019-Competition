3b8a00a533bcda94c09b9a9220aa14843ba54c6c
Juno Ryan <juno@Junos-MacBook-Air.local>
2019-01-25 18:52:38 -0500

    created and deadzoned arcade controls

---
+++ robot.py
@@ -0,0 +1,42 @@
+import wpilib
+
+LEFT = GenericHID.Hand.kLeft
+RIGHT = GenericHID.Hand.kRight
+
+class Robot(wpilib.IterativeRobot):
+    def robotInit(self):
+        #assigns driver as controller 0 and operator as controller 1
+
+        self.driver = wpilib.XboxController(0)
+        self.operator = wpilib.XboxController(1)
+
+    def robotPeriodic(self):
+
+    def teleopInit(self):
+
+    def teleopPeriodic(self):
+        #Arcade Controls
+
+        DEADZONE = 0.2
+        MAX_ACCELERATION = 0.3
+
+        goal_forward = -self.driver.getY(RIGHT)
+        goal_rotate = self.driver.getX(LEFT)
+
+        MAX_FORWARD = 1.0
+        MAX_ROTATE = 1.0
+
+        goal_forward = deadzone(goal_forward * MAX_ROTATE, DEADZONE)
+
+        delta = goal_forward - self.forward
+
+        if abs(delta) < MAX_ACCELERATION:
+            self.forward += delta
+        else:
+            self.forward += MAX_ACCELERATION
+
+#prevents slight movements of triggers from moving robot
+def deadzone(val, deadzone):
+    if abs(val) < deadzone:
+        return 0
+    return val
