89eca620d7d286024b8bb24a7b744485def10a81
Danniell Hu <dannihu01@gmail.com>
2019-01-25 19:20:02 -0500

    Elevator init

---
+++ subsystems/elevator.py
@@ -0,0 +1,14 @@
+import wpilib
+
+class Elevator:
+    def __init__(self, motor):
+        self.motor = motor
+
+    def go_up(self, speed = 1.0):
+        self.motor.set(speed)
+
+    def go_down(self, speed = 1.0):
+        self.motor.set(-speed)
+
+    def stop(self)
+        self.motor.set(0)
