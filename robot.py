ff8ae819416b83f9027e45a1d66411f65cd0adb1
Austin <austinjvetor@gmail.com>
2019-01-25 20:41:04 -0500

    Circular Deadzone implementation

--- robot.py
+++ robot.py
@@ -1,5 +1,7 @@
-import wpilib
-
+import math
+import wpilib 
+from wpilib.interfaces import GenericHID
+ 
 LEFT = GenericHID.Hand.kLeft
 RIGHT = GenericHID.Hand.kRight
 
@@ -11,8 +13,10 @@ class Robot(wpilib.IterativeRobot):
         self.operator = wpilib.XboxController(1)
 
     def robotPeriodic(self):
+        pass
 
     def teleopInit(self):
+        pass
 
     def teleopPeriodic(self):
         #Arcade Controls
@@ -26,17 +30,47 @@ class Robot(wpilib.IterativeRobot):
         MAX_FORWARD = 1.0
         MAX_ROTATE = 1.0
 
-        goal_forward = deadzone(goal_forward * MAX_ROTATE, DEADZONE)
+  
+    
+        (x,y) = deadzone2(DEADZONE, (goal_rotate, goal_forward))
+def is_in_deadzone(radius, location):
+    (x,y) = location
+    """
+    #if in the function is in deadzone it will return true.
+    #if the function is outside of the deadzone the function will return false.
+    """
+    return(x**2+y**2) <= (radius**2)
+  
+def deadzone2(radius, location):
+    """
+    # if in the deazone this function will return 0,0.
+    """
+    (x,y) = location
+    if is_in_deadzone(radius, location):
+        return(0,0)
+    elif location[1] == 0:
+        """
+        # if x is zero this function will just y without mutiplying or divideing by 0.
+        """
+        if x < 0:
+            nx = (x + radius)/(1-radius)
+        else:  
+            nx = (x - radius)/(1 - radius)
+            nx = round(nx, 2)
+        return(nx,0)
 
-        delta = goal_forward - self.forward
+      
+    else:
+        """
+        # Rescales the x and the y values for the deadzone.
+        will turn the x and y to polar cordinates and rescale then then turn them back into the rescaled cartesian coordinates.
+        """
+        i = (x**2+y**2)
+        r = (math.sqrt(i))
+        theta = (math.atan(x/y))
+        nx = (r*math.cos(theta))
+        nx = round(nx, 2)
+        ny = (r*math.sin(theta))
+        ny = round(ny, 2)
+    return(nx,ny)
 
-        if abs(delta) < MAX_ACCELERATION:
-            self.forward += delta
-        else:
-            self.forward += MAX_ACCELERATION
-
-#prevents slight movements of triggers from moving robot
-def deadzone(val, deadzone):
-    if abs(val) < deadzone:
-        return 0
-    return val
