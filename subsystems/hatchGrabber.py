9069bad76b5b4af877a519c16b12f229c1137fe7
Danniell Hu <dannihu01@gmail.com>
2019-01-25 19:43:13 -0500

    Hatch grabber init

---
+++ subsystems/hatchGrabber.py
@@ -0,0 +1,26 @@
+from wpilib import DoubleSolenoid
+
+class Grabber:
+    def __init__(self, retract, extend)
+        self.out = SolenoidPair(retract, extend)
+
+    def lower_down(self):
+        self.out.retract()
+    
+    def raise_up(self):
+        self.out.extend()
+
+class SolenoidPair:
+    def __init__(self, retract, extend):
+        self._retract = retract
+        self._extend = extend 
+    
+    def retract(self):
+        self._retract.set(True)
+        self._extend.set(False)
+
+    def extend(self):
+        self._extend.set(True)
+        self._retract.set(False)
+
+
