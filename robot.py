abde77f7cd3317b2bcec677e0c597e6d68768cf5
Danniell Hu <dannihu01@gmail.com>
2019-01-31 19:13:39 -0500

    Fix the name of the createMasterAndSlaves function

--- robot.py
+++ robot.py
@@ -36,8 +36,8 @@ class MyRobot(wpilib.IterativeRobot):
         self.gyro = wpilib.AnalogGyro(1)
 
         #DRIVETRAIN
-        left = setMasterAndSlaves(LEFT_MASTER_ID, LEFT_SLAVE_1_ID, LEFT_SLAVE_2_ID)
-        right = setMasterAndSlaves(RIGHT_MASTER_ID, RIGHT_SLAVE_1_ID, RIGHT_SLAVE_2_ID)
+        left = createMasterAndSlaves(LEFT_MASTER_ID, LEFT_SLAVE_1_ID, LEFT_SLAVE_2_ID)
+        right = createMasterAndSlaves(RIGHT_MASTER_ID, RIGHT_SLAVE_1_ID, RIGHT_SLAVE_2_ID)
         self.drivetrain = Drivetrain(left, right, self.gyro)
 
         #HATCH GRABBER
@@ -47,7 +47,8 @@ class MyRobot(wpilib.IterativeRobot):
             )
         
         #ELEVATOR
-        self.elevator = ctre.WPI.TalonSRX(ELEVATOR_ID)
+        elevator_motor = ctre.WPI_TalonSRX(ELEVATOR_ID)
+        self.elevator = Elevator(elevator_motor, encoder_motor=elevator_motor)
        
     def robotPeriodic(self):
         pass
@@ -80,7 +81,16 @@ class MyRobot(wpilib.IterativeRobot):
         self.robot_drive.arcade_drive(self.forward, rotation_value)
 
         #ELEVATOR CONTROL
-
+        '''
+        Guitar Hero controls
+        1: Hatch Panel Low. 1+Wammy: Cargo Low (1, z!=0)
+        2: Hatch Panel Middle. 2+wammy: Cargo Middle (2, z!=0)
+        3: Hatch Panel High. 3+wammy: Cargo High (4, z!=0)
+        (1-3 elevator positions = 2 CIM motors in a toughbox gearbox)
+        4: Cargo intake IN. 4+wammy: Cargo intake out (single motor tbd) (3, z!=0)
+        5: Hatch Panel grab (piston out). 5+wammy: Hatch Panel release (piston in). (5, z!=0)
+        Start: Activate end game with Driver approval (8)
+        '''
 
         #END GAME 
 
@@ -103,28 +113,23 @@ class MyRobot(wpilib.IterativeRobot):
         else:
             return -1
 
-    def setMasterAndSlaves(MASTER, slave1, slave2):
+
+
+    def createMasterAndSlaves(MASTER, slave1, slave2):
+        '''
+        First ID must be MASTER, Second ID must be slave TALON, Third ID must be slave VICTOR
+        This assumes that the left and right sides are the same, two talons and one victor. A talon must be the master.
+        '''
         master_talon = ctre.WPI_TalonSRX(MASTER)
-        slave_talon_1 = CANTalon(slave1)
-        slave_talon_2 = CANTalon(slave2)
 
-        slave_talon_1.changeControlMode(ControlMode.Follower)
-        slave_talon_1.set(MASTER)
+        slave_talon = ctre.WPI_TalonSRX(slave1)
+        slave_victor = ctre.victorspx.VictorSPX(slave2)
 
-        slave_talon_2.changeControlMode(ControlMode.Follower)
-        slave_talon_2.set(MASTER)
+        slave_talon.follow(master_talon)
+        slave_victor.follow(master_talon)
 
+        return master_talon
 
-        '''
-        Guitar Hero controls
-        1: Hatch Panel Low. 1+Wammy: Cargo Low (1, z!=0)
-        2: Hatch Panel Middle. 2+wammy: Cargo Middle (2, z!=0)
-        3: Hatch Panel High. 3+wammy: Cargo High (4, z!=0)
-        (1-3 elevator positions = 2 CIM motors in a toughbox gearbox)
-        4: Cargo intake IN. 4+wammy: Cargo intake out (single motor tbd) (3, z!=0)
-        5: Hatch Panel grab (piston out). 5+wammy: Hatch Panel release (piston in). (5, z!=0)
-        Start: Activate end game with Driver approval (8)
-        '''
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
