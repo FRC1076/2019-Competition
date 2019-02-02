63501e6c4b40a3fe63442c95e09ba5bcc3851ff0
Danniell Hu <dannihu01@gmail.com>
2019-02-01 22:38:47 -0500

    Fix a lot of syntax errors

--- subsystems/lift.py
+++ subsystems/lift.py
@@ -2,26 +2,24 @@ import wpilib
 from wpilib import DoubleSolenoid
 
 class Lift:
-
     def __init__(self, 
     front_left_retract, front_left_extend,
     front_right_retract, front_right_extend,
     back_left_retract, back_left_extend,
-    back_right_retract, back_right_extend
-    )
+    back_right_retract, back_right_extend):
 
-    self.front_left = SolenoidPair(front_left_retract, front_left_extend)
-    self.front_right = SolenoidPair(front_right_retract, front_right_extend)
-    self.back_left = SolenoidPair(back_left_retract, back_left_extend)
-    self.back_right = SolenoidPair(back_right_retract, back_right_extend)
+        self.front_left = SolenoidPair(front_left_retract, front_left_extend)
+        self.front_right = SolenoidPair(front_right_retract, front_right_extend)
+        self.back_left = SolenoidPair(back_left_retract, back_left_extend)
+        self.back_right = SolenoidPair(back_right_retract, back_right_extend)
 
-    def lower_all(self)
+    def lower_all(self):
         self.front_left.retract()
         self.front_right.retract()
         self.back_left.retract()
         self.back_right.retract()
 
-    def raise_all(self)
+    def raise_all(self):
         self.front_left.extend()
         self.front_right.extend()
         self.back_left.extend()
