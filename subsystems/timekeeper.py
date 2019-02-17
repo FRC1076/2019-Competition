18a6ee1587ce3a888a18d54b6830d17289307363
primevaldragon <austinjvetor@gmail.com>
2019-02-17 17:20:43 -0500

    fixing timer starts

--- subsystems/timekeeper.py
+++ subsystems/timekeeper.py
@@ -5,14 +5,15 @@ class MatchTimer:
 
     def __init__(self, duration):
         self.duration = duration
-        self.MatchTime = wpilib.Timer()
-        self.MatchTime.reset()
-        self.MatchTime.start()
-        self.duration = duration    
+        
+        self.MatchTime = wpilib.Timer()          
         self.NotifyTimer = wpilib.Timer()
+        
+        self.MatchTime.reset()
         self.NotifyTimer.reset()
+        
         self.NotifyTimer.start()
-
+        self.MatchTime.start()
     def time_left(self):     
         if self.NotifyTimer.hasPeriodPassed(1):
             self.NotifyTimer.reset()
