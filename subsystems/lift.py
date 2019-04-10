2add369fa08934c0847a766e92f45bdb2c73cad0
Danniell Hu <dannihu01@gmail.com>
2019-04-10 16:52:57 -0400

    Delete front pistons for Lift because they don't exist anymore

--- subsystems/lift.py
+++ subsystems/lift.py
@@ -9,26 +9,12 @@ class Lift:
     stateExtend = DoubleSolenoid.Value.kForward
     stateRetract = DoubleSolenoid.Value.kReverse
 
-    def __init__(self, center, back):
-        self.center = center
+    def __init__(self, back):
+        #self.center = center
         self.back = back
 
-    def raise_center(self):
-        self.center.set(Lift.stateExtend)
-    
     def raise_back(self):
         self.back.set(Lift.stateExtend)
 
-    def lower_center(self):
-        self.center.set(Lift.stateRetract)
-
     def lower_back(self):
         self.back.set(Lift.stateRetract)
-
-    def lower_all(self): 
-        self.lower_center()
-        self.lower_back()
-
-    def raise_all(self):
-        self.raise_center()
-        self.raise_back()
