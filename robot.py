4d101b02dd4ef1212c75fc3ecd26e0eaec52c180
Matthew Jones <mcolinj@mac.com>
2019-04-20 15:48:00 -0400

    Ken Bone Vision PID NetworkTables integration

--- robot.py
+++ robot.py
@@ -84,24 +84,13 @@ SERVO3_CHANNEL = 3 #back right
 DOWN_SONAR_TRIGGER_PIN = 4
 DOWN_SONAR_ECHO_PIN = 5
 
-TOLERANCE_DEGREES = 2
-
-'''
-Raw Axes
-0 L X Axis
-1 L Y Axis
-2 L Trigger
-3 R Trigger
-4 R X Axis
-5 R Y Axis
-'''
+TOLERANCE_DEGREES = 1
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
         NetworkTables.initialize()
         self.sd = NetworkTables.getTable("SmartDashboard")
 
-        
         #assigns driver as controller 0 and operator as controller 1
         self.driver = wpilib.XboxController(0)
         self.operator = wpilib.XboxController(1)
@@ -115,75 +104,26 @@ class MyRobot(wpilib.TimedRobot):
         right = createTalonAndSlaves(RIGHT_MASTER_ID, RIGHT_SLAVE_1_ID)
         self.drivetrain = Drivetrain(left, right, None)
 
-        #HATCH GRABBER
-        #self.grabber = Grabber(
-        #    hatch = wpilib.DoubleSolenoid(PCM_CAN_ID, EXTEND_ID, RETRACT_ID))
-
-        #ball manipulator and controller
-        self.ballManipulator = BallManipulator(ctre.WPI_TalonSRX(BALL_MANIP_ID))
-        self.ballManipulatorController = BallManipulatorController(self.operator, self.logger)
-
-        #EXTEND HATCH GRABBER 
-        #LIFT
-        '''
-        The lift is being controlled by four pistons, but two doublesolenoids due to electrical chaining 4 together to make
-        space on the solenoid module.
-        '''
-        #ELEVATOR
-        #elevator_motor = createTalonAndSlaves(ELEVATOR_ID_MASTER, ELEVATOR_ID_SLAVE)
-        #elevator_motor = ctre.WPI_TalonSRX(ELEVATOR_ID_MASTER)
-        #self.elevator = Elevator(elevator_motor, encoder_motor=elevator_motor)
-        #.WPI_TalonSRX
-        #self.ahrs = AHRS.create_spi()
-        # down-facing sonar unit
-
-        # remote sensors
         #Vision sensor
-        self.visionSensor = VisionSensor(self.sd, logger=self.logger)
-        #SERVO
-        self.servo0 = ContinuousRotationServo(SERVO0_CHANNEL)
-        self.servo1 = ContinuousRotationServo(SERVO1_CHANNEL)
-        self.servo2 = ContinuousRotationServo(SERVO2_CHANNEL)
-        self.servo3 = ContinuousRotationServo(SERVO3_CHANNEL)
-        #self.servo2 = ContinuousRotationServoWithFeedback(7, 3)
-        
-
+        self.visionSensor = VisionSensor(self.sd, self.logger)
+        self.visionAttendant = VisionAttendant(self.visionSensor, self.logger)
         self.timer = 0
-
-        #Sonar sensor
-        #self.sonarSensor = SonarSensor('10.10.76.11', 5811, logger=self.logger)
-
-        # Elevator height sonar sensor
-        #self.elevatorHeightSensor = SonarSensor('10.10.76.11', 5811, logger=self.logger)
-        #self.elevatorAttendant = ElevatorAttendant(self.elevatorHeightSensor, 0, 200, -0.5, 1.0)
-
-        self.visionAttendant = VisionAttendant(self.visionSensor)
-
-        self.autoBalancing = False
-
-        
-
 
     def robotPeriodic(self):
         # if self.timer % 50 == 0:
         #     print("NavX Gyro Roll", self.gyro.getRoll())
         self.timer += 1
 
+
     def teleopInit(self):
         """Executed at the start of teleop mode"""
         self.forward = 0
-
         self.visionAttendant.initialize()
-        
+
     def teleopPeriodic(self):
 
         #print("NavX Gyro Roll, ", self.gyro.getRoll(), "NavX Gyro Pitch", self.gyro.getPitch())
-         
-        
-        
-
         #ARCADE DRIVE CONTROL
-        
         deadzone_value = 0.2
         max_accel = 0.15
         max_forward = 1.0
@@ -197,7 +137,7 @@ class MyRobot(wpilib.TimedRobot):
 
         # # manual and autonomous driving will go here
         if self.driver.getBButton():
-            self.logger.info("Button B pressed, turn to target!")
+            self.logger.error("Button B pressed, turn to target!")
             self.visionAttendant.setSetpoint(0)
             self.visionAttendant.move()
             # if we are auton turning, we can override value with pid
@@ -206,11 +146,6 @@ class MyRobot(wpilib.TimedRobot):
             self.visionAttendant.stop()
             rotation_value = deadzone(rotation_value, deadzone_value) * max_rotate
             
-        # if self.driver.getTriggerAxis(RIGHT_CONTROLLER_HAND):
-        #     self.drivetrain.arcade_drive((self.forward/2), (rotation_value*0.75))
-        # else:
-        #     self.drivetrain.arcade_drive(self.forward, rotation_value)
-
         delta = goal_forward - self.forward
 
         if abs(delta) < max_accel:
@@ -218,30 +153,9 @@ class MyRobot(wpilib.TimedRobot):
         else:
             self.forward += max_accel * sign(delta)
 
-        #If the driver holds the right trigger down, we will go half speed forward 
-        # and backward and 75% speed when turning.
-        if self.driver.getTriggerAxis(RIGHT_CONTROLLER_HAND) > 0.35:
-            self.drivetrain.arcade_drive((self.forward/2), (rotation_value*0.75))
-        else:
-           self.drivetrain.arcade_drive(self.forward, rotation_value) 
-        # self.drivetrain.arcade_drive(goal_forward, rotation_value)
+        self.drivetrain.arcade_drive(self.forward, rotation_value) 
 
-        #4BAR CONTROL
-        #END GAME 
-        whammyAxis = self.operator.getRawAxis(4)
-        whammy_down = (whammyAxis > -0.7 and not (whammyAxis == 0))
 
-        driver_activate = self.driver.getYButton() and self.driver.getBButton()
-        #driver_activate_center = self.driver.getBButton() and self.driver.getStartButton()
-
-        activate_pistons = driver_activate and whammy_down
-        release_back_pistons = self.driver.getBackButton() 
-        #release_center_pistons = self.driver.getStartButton()
-
-        #The front (center) pistons will fire 0.25 seconds after the back pistons have been fired.
-
-        # if self.autoBalancing == True:
-        #     self.climber.balanceMe()
     def autonomousInit(self):
         #Because we want to drive during auton, just call the teleopInit() function to 
         #get everything from teleop.
@@ -275,28 +189,28 @@ class VisionAttendant:
         #self.network_tables = network_tables
         self.turnRate = 0
 
-        kP = 0.01
+        kP = 0.0282
         kI = 0.0
         kD = 0
 
         self.pid = wpilib.PIDController(kP, kI, kD, source=self.vision_sensor, output=self, period=0.05)
-        self.pid.setInputRange(-30, 30)
+        self.pid.setInputRange(-25, 25)
         self.pid.setOutputRange(-.5, .5)
         self.pid.setAbsoluteTolerance(TOLERANCE_DEGREES)
 
-        # self.sdWidget = wpilib.SmartDashboard.getData("Vision-Controller")
-    def initialize(self):
         wpilib.SmartDashboard.putData("Vision-Controller", self.pid)
+
+        # self.sdWidget = wpilib.SmartDashboard.getData("Vision-Controller")
+
+    def initialize(self): 
         self.sdWidget = wpilib.SmartDashboard.getData("Vision-Controller")
 
-
-        kP = self.sdWidget.getP()
-        kI = self.sdWidget.getI()
-        kD = self.sdWidget.getD()
+        self.pid.setP(self.sdWidget.getP())
+        self.pid.setI(self.sdWidget.getI())
+        self.pid.setD(self.sdWidget.getD())
         
         if self.logger is not None:
-            self.logger.info("Got kP %f " , kP)
-
+            self.logger.error("Got kP %f ", self.pid.getP())
 
     def pidWrite(self, output):
         self.turnRate = output
@@ -339,10 +253,10 @@ def deadzone(val, deadzone):
         return (x)
 
 def sign(number):
-    if number > 0:
-        return 1
-    else:
+    if number < 0:
         return -1
+    else:
+        return 1
         
 if __name__ == "__main__":
     wpilib.run(MyRobot)
--- subsystems/visionSensor.py
+++ subsystems/visionSensor.py
@@ -2,17 +2,8 @@ import wpilib
 import json
 from networktables import NetworkTables
 
-
 class VisionSensor:
     """
-    Vision sensor has two main roles.   It listens on the specified
-    port for udp angle packets and parses them to obtain the most
-    recent value for the angle to target.  It also acts as a data
-    source for a PIDController.
-
-    If the target is to the left of the robot, the angle is reported
-    as negative.
-
     Example:
         visionSensor = VisionSensor('10.10.76.7', 8812)
 
@@ -21,27 +12,12 @@ class VisionSensor:
     def __init__(self, network_table, logger=None):
         self.network_table = network_table
         self.logger = logger
-        
-        
-        self.bearing = 0
-        self.range_cm = 0
-        
-
-    def pidGetBearing(self):
-        # return cached bearing that was last received from
-        # the vision system
-        return self.bearing
-
-    def pidGetRange(self):
-        # return cached range that was last received from
-        # the vision system
-        return self.range_cm
 
     def pidGet(self):
-        return self.pidGetBearing()
+        bearing = self.network_table.getNumber('vision-bearing', 0.0)
+        if self.logger is not None:
+            self.logger.error('Got vision-bearing of ',bearing)
+        return bearing
 
     def getPIDSourceType(self):
         return wpilib.interfaces.pidsource.PIDSource.PIDSourceType.kDisplacement
-
-    def receiveAngleUpdates(self):
-        self.bearing = self.network_tble.getNumber('vision-bearing', 0.0)
